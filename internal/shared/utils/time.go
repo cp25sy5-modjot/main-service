df228a12e0346e6421dc55bb4d6a025906728259
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2026-01-13 00:46:27 +0700

    change date filter

--- internal/shared/utils/time.go
+++ internal/shared/utils/time.go
@@ -2,59 +2,89 @@ package utils
 
 import "time"
 
+// func GetStartAndEndOfMonth(t time.Time) (time.Time, time.Time) {
+// 	loc := t.Location()
+
+// 	// First day of current month at 00:00
+// 	firstOfCurrent := time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc)
+
+// 	// Last day of previous month = firstOfCurrent - 1 day
+// 	lastOfPrevious := firstOfCurrent.AddDate(0, 0, -1)
+
+// 	// Last day of current month = first of next month - 1 day
+// 	firstOfNext := firstOfCurrent.AddDate(0, 1, 0)
+// 	lastOfCurrent := firstOfNext.AddDate(0, 0, -1)
+
+// 	// Set final times to 17:00:00
+// 	startRange := time.Date(
+// 		lastOfPrevious.Year(), lastOfPrevious.Month(), lastOfPrevious.Day(),
+// 		17, 0, 0, 0,
+// 		loc,
+// 	)
+
+// 	endRange := time.Date(
+// 		lastOfCurrent.Year(), lastOfCurrent.Month(), lastOfCurrent.Day(),
+// 		17, 0, 0, 0,
+// 		loc,
+// 	)
+
+// 	return startRange, endRange
+// }
+
+// func GetStartAndEndOfPreviousMonth(t time.Time) (time.Time, time.Time) {
+// 	loc := t.Location()
+
+// 	// First day of current month at 00:00
+// 	firstOfCurrent := time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc)
+
+// 	// Last day of previous month = firstOfCurrent - 1 day
+// 	lastOfPrevious := firstOfCurrent.AddDate(0, 0, -1)
+
+// 	// First day of previous month
+// 	firstOfPrevious := time.Date(lastOfPrevious.Year(), lastOfPrevious.Month(), 1, 0, 0, 0, 0, loc)
+
+// 	// Set final times to 17:00:00
+// 	startRange := time.Date(
+// 		firstOfPrevious.Year(), firstOfPrevious.Month(), firstOfPrevious.Day(),
+// 		17, 0, 0, 0,
+// 		loc,
+// 	)
+
+// 	endRange := time.Date(
+// 		lastOfPrevious.Year(), lastOfPrevious.Month(), lastOfPrevious.Day(),
+// 		17, 0, 0, 0,
+// 		loc,
+// 	)
+
+// 	return startRange, endRange
+// }
+
 func GetStartAndEndOfMonth(t time.Time) (time.Time, time.Time) {
 	loc := t.Location()
 
-	// First day of current month at 00:00
-	firstOfCurrent := time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc)
-
-	// Last day of previous month = firstOfCurrent - 1 day
-	lastOfPrevious := firstOfCurrent.AddDate(0, 0, -1)
-
-	// Last day of current month = first of next month - 1 day
-	firstOfNext := firstOfCurrent.AddDate(0, 1, 0)
-	lastOfCurrent := firstOfNext.AddDate(0, 0, -1)
-
-	// Set final times to 17:00:00
-	startRange := time.Date(
-		lastOfPrevious.Year(), lastOfPrevious.Month(), lastOfPrevious.Day(),
-		17, 0, 0, 0,
+	start := time.Date(
+		t.Year(), t.Month(), 1,
+		0, 0, 0, 0,
 		loc,
 	)
 
-	endRange := time.Date(
-		lastOfCurrent.Year(), lastOfCurrent.Month(), lastOfCurrent.Day(),
-		17, 0, 0, 0,
-		loc,
-	)
+	end := start.AddDate(0, 1, 0)
 
-	return startRange, endRange
+	return start, end
 }
 
 func GetStartAndEndOfPreviousMonth(t time.Time) (time.Time, time.Time) {
 	loc := t.Location()
 
-	// First day of current month at 00:00
-	firstOfCurrent := time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc)
-
-	// Last day of previous month = firstOfCurrent - 1 day
-	lastOfPrevious := firstOfCurrent.AddDate(0, 0, -1)
-
-	// First day of previous month
-	firstOfPrevious := time.Date(lastOfPrevious.Year(), lastOfPrevious.Month(), 1, 0, 0, 0, 0, loc)
-
-	// Set final times to 17:00:00
-	startRange := time.Date(
-		firstOfPrevious.Year(), firstOfPrevious.Month(), firstOfPrevious.Day(),
-		17, 0, 0, 0,
+	// First day of current month
+	firstOfCurrent := time.Date(
+		t.Year(), t.Month(), 1,
+		0, 0, 0, 0,
 		loc,
 	)
 
-	endRange := time.Date(
-		lastOfPrevious.Year(), lastOfPrevious.Month(), lastOfPrevious.Day(),
-		17, 0, 0, 0,
-		loc,
-	)
+	start := firstOfCurrent.AddDate(0, -1, 0)
+	end := firstOfCurrent
 
-	return startRange, endRange
+	return start, end
 }
